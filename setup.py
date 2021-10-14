cef986bad49ba641a1ec45e15e8d8cdcf686ed3e
anthony-freitas <78743473+anthony-freitas@users.noreply.github.com>
2021-10-14 17:26:42 -0300

    dasdasd

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from distutils.core import setup
 
 setup(
-    name='py_misc',
-    version='1.3.1',
+    name='py_wapp',
+    version='1.0.7',
     license='MIT',
-    description='Miscellaneous Library to Simplify Python Code',
+    description='Python Miscellaneous Library',
     author='anthony',
     url='https://github.com/anthony-freitas/py-misc',
     packages=[
