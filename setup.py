c31b85369f9775f7fa246dda8c4e75d780724dc3
anthony-freitas <78743473+anthony-freitas@users.noreply.github.com>
2021-10-25 16:16:11 -0300

    asdasd

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(
     description='HTTP Rest API for Whatsapp Bot',
     author='anthony',
     url='https://github.com/anthony-freitas/py-wapp',
-    packages=['py_wapp']
+    packages=['py_wapp', 'py_wapp/modules']
 )
