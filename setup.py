30f87d53aac60ca8a6cee58fe90e79dcf19133b6
anthony-freitas <78743473+anthony-freitas@users.noreply.github.com>
2021-10-27 09:05:45 -0300

    adding dependencies

--- setup.py
+++ setup.py
@@ -4,10 +4,16 @@ from distutils.core import setup
 
 setup(
     name='py_wapp',
-    version='1.0.7',
+    version='2.0.7',
     license='MIT',
     description='HTTP Rest API for Whatsapp Bot',
     author='anthony',
     url='https://github.com/anthony-freitas/py-wapp',
-    packages=['py_wapp', 'py_wapp/modules']
+    packages=[
+        'py_wapp',
+        'py_wapp/modules'
+    ],
+    install_requires=[
+        'py_misc @ git+https://github.com/anthony-freitas/py-misc#egg=py-misc'
+    ]
 )
